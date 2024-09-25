d1c9af262a9267f21fc0c0ee79bda75fcc42968c
isabellecarvalho0 <141517104+isabellecarvalho0@users.noreply.github.com>
2024-09-25 10:21:53 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+// Código do UserWay
+(function(d) {
+    var s = d.createElement("script");
+    s.setAttribute("data-account", "SEU_ID_AQUI");
+    s.setAttribute("src", "https://cdn.userway.org/widget.js");
+    (d.body || d.head).appendChild(s);
+})(document);
